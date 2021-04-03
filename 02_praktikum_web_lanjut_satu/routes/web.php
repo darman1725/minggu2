3ae880d119c69b2b34c583936f776f7dafd9ff5e
darman1725 <darmansaragih1701@gmail.com>
2021-04-03 09:32:46 +0700

    Praktikum 1

---
+++ 02_praktikum_web_lanjut_satu/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/a', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', 'App\Http\Controllers\WelcomeController@hello');
+
+Route::get('/', function() {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function() {
+    return 'Nama : Darman Saputra Saragih <br> NIM : 1941720060';
+});
+
+Route::get('/articles/{id}', function($id) {
+    return 'Halaman Artikel dengan id '.$id;
+});
+
